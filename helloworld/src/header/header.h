3d5619b0f7f15468c81471fae1cc99b6c6849d1a
rdouglas <rdouglas8906@gmail.com>
2024-05-01 16:53:20 -0500

    layout: directory layout follows src convention

---
+++ helloworld/src/header/header.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_H_
+#define HEADER_H_
+
+// libraries get their own namespace to avoid name collisions
+namespace header {
+
+void print();
+
+}  // namespace header
+
+#endif  // HEADER_H_
